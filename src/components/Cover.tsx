b9a5a6baa89133592d57fa84edf6493ded836850
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-12-30 13:56:39 +0100

    name completed

---
+++ src/components/Cover.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cover = () => {
+  return (
+    
+      <div className="flex justify-center align-item-center">
+        <div className='m-8 p-12 border-2 border-white text-white'>
+            Helena González González
+            </div>
+      </div>
+   
+  );
+};
+
+export default Cover;
