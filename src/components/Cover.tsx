745b594ebd6460bc03dd07a2cd10f663f2a3537f
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-01-28 14:27:40 +0100

    created a new font

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Cover = () => {
   return (
-    
-      <div className="flex justify-center align-item-center">
-        <div className='m-8 p-12 border-2 border-white text-white'>
-            Helena González González
-            </div>
+    <div className='flex justify-center align-item-center'>
+      <div className='mt-8 p-6 pt-0 pr-0 border-l-2 border-b-2 animate-color-change-outside border-white rounded-sm'>
+        <div className='p-20 border-l-2 border-b-2 animate-color-change border-white font-moirai font-bold text-5xl text-white'>
+          Helena González González
+        </div>
       </div>
-   
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'color-change': 'color-change 2s infinite',
+        'color-change-outside': 'color-change 2s infinite',
+      },
+      fontFamily: {
+        'moirai': ['Moirai One', 'system-ui']
+      }
+    },
   },
   variants: {
     extend: {},
